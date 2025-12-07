36893e20451b51e8faa547584227109c1674e52c
MohamedJilali <mohamed.el.jilali.abdelkader@student.howest.be>
2025-12-07 16:06:23 +0100

    sanitize fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
+import { sanitizeHtml } from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
 
 const parser = new MarkdownIt();

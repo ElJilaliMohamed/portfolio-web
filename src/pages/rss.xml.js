488cff445098aa8d3c5345ca18f5b243c0d41385
MohamedJilali <mohamed.el.jilali.abdelkader@student.howest.be>
2025-12-07 21:33:18 +0100

    mini error build fixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,13 +2,11 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
-
 const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('aboutMe');
-
-  const feed = await rss({
+  return rss({
     stylesheet: '/rss/styles.xsl',
     title: 'cool stuff',
     description: 'My journey learning Astro',
@@ -16,11 +14,12 @@ export async function GET(context) {
     items: posts.map((post) => ({
       title: post.data.title,
       description: post.data.description,
-      link: `/portfolio-web/blog/${post.id}`,
+      link: `portfolio-web/blog/${post.id}`,
       content: sanitizeHtml(parser.render(post.body), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
       }),
     })),
     customData: `<language>en-us</language>`,
   });
-}   
+  
+}

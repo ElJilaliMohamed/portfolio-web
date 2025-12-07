718769050f324e228487091230dc7cbf8de19642
MohamedJilali <mohamed.el.jilali.abdelkader@student.howest.be>
2025-12-07 20:52:52 +0100

    still fixing rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('aboutMe');
@@ -11,7 +14,10 @@ export async function GET(context) {
     items: posts.map((post) => ({
       title: post.data.title,
       description: post.data.description,
-      link: post.url,
+      link: `portfolio-web/blog/${post.id}`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
     })),
     customData: `<language>en-us</language>`,
   });
